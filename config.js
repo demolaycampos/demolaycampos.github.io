c8cfdbfff3f01c15d7052a8b7fb029418b21eb73
demolaycampos <contato.cg355@gmail.com>
2026-03-06 12:31:02 -0300

    https://

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // Edite APENAS este arquivo para alterar credenciais
 // ═══════════════════════════════════════════════════════
 const CONFIG = {
-    PROXY_URL: 'old-sun-9202.contato-cg355.workers.dev',
+    PROXY_URL: 'https://old-sun-9202.contato-cg355.workers.dev',
     CLIENT_ID: '173684456547-n01u603usrbocdb4mgb5potr5uhkhr1u.apps.googleusercontent.com',
     SHEET_ID: '1lyyBGpjuNZdvHID3Iqte89DmilsSn8Ovq1zueazMG98',
     SCOPES: 'https://www.googleapis.com/auth/spreadsheets profile email',
